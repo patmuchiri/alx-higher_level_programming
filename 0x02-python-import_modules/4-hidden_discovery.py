72aa931b25f0c48f3b64ada7ede09c5a0dac56e5
patrickmuchiri <pmuchirig@gmail.com>
2023-11-02 14:46:59 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,9 @@
-ll
+#!/usr/bin/python3
+import hidden_4  # Import the module
+
+# List the names defined in the imported module
+module_names = dir(hidden_4)
+print("module_names")
+for name in module_names:
+    if name.isidentifier():
+        print(name)
