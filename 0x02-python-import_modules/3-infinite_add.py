662d8196e7da3aabf4d3230bcfbd0b502f3c4a8c
patrickmuchiri <pmuchirig@gmail.com>
2023-11-02 13:47:09 -0700

    3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    total = 0
+
+    
+    for arg in args:
+        try:
+            num = int(arg)
+            total += num
+        except ValueError:
+            print(f"Error: '{arg}' is not a valid integer.")
+
+    print(total)
