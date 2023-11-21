3352c8570ae85ec843ece6d3fc3efe95f872b6bb
patrickmuchiri <pmuchirig@gmail.com>
2023-11-21 05:05:41 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,5 +8,7 @@ def safe_print_list(my_list=[], x=0):
         print()
         return printed_elements
     except IndexError:
+        pass
+    finally:
         print()
         return printed_elements
