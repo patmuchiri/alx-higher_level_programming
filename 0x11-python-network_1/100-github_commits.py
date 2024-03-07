aec4f50fb1cc59dc00a33b450552220ef760a67d
patrickmuchiri <pmuchirig@gmail.com>
2024-03-06 17:17:24 -0800

    100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Time for an interview """
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+
+    res = requests.get(url)
+
+
+    try:
+        data = res.json()
+        for i, obj in enumerate(data):
+            print('{}: {}'.format(obj.get('sha'),
+                                  obj.get('commit').get('author').get('name')))
+            if i == 9:
+                break
+    except Exception:
+        pass
